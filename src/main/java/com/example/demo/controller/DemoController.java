42679cc3317ec8ef0c4a07115deade028082ebbf
Tanwi Roy (DevOn) <t.roy@devon.nl>
2024-06-01 16:51:33 +0530

    changes

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,8 @@ public class DemoController {
 
     @GetMapping("/api/test")
     public String testEndpoint() {
-        return "Welcome to Spring Application";
+        String str = "Welcome to Spring Application";
+        return str;
     }
 
 }
