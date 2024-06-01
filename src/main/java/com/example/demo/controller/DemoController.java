0794158d2ad75676abee9bc9122dcc0a10cd56d3
Tanwi Roy (DevOn) <t.roy@devon.nl>
2024-06-01 15:14:32 +0530

    Creating endpoint

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/api/test")
+    public String testEndpoint() {
+        return "Welcome to Spring Application";
+    }
+}
